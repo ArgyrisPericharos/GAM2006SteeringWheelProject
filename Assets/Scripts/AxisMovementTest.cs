5d3fe3c0ea8ecb175d1bad83e2fd6839e11fee25
ArgyrisPericharos <ap1747@live.mdx.ac.uk>
2023-03-10 12:23:47 +0000

    Worked!!

--- Assets/Scripts/AxisMovementTest.cs
+++ Assets/Scripts/AxisMovementTest.cs
@@ -5,28 +5,40 @@ using UnityEngine.InputSystem;
 
 public class AxisMovementTest : MonoBehaviour
 {
-    public InputAction playerControl;
-    // Start is called before the first frame update
+    public float moveSpeed = 5f;
+    public InputControl inputControl;
+    public InputAction playerControls;
+
+    public float current;
+
+
+
+
+    // Start is called before the first frame updat
+
     private void OnEnable()
     {
-        playerControl.Enable();
+        playerControls.Enable();
     }
 
     private void OnDisable()
     {
-        playerControl.Disable();
+        playerControls.Disable();
     }
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        
+        current = playerControls.ReadValue<float>();
 
+        transform.Translate(current * moveSpeed, 0, 0);
         
     }
+
+
 }
